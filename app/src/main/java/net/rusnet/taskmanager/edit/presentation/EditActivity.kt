3d856eda695c9842fa4bbe3b4987e2aa0bbd2617
Roman Kamyshnikov <rkam88@gmail.com>
2020-05-07 20:03:17 +0300

    Add EditActivity

---
+++ app/src/main/java/net/rusnet/taskmanager/edit/presentation/EditActivity.kt
@@ -0,0 +1,13 @@
+package net.rusnet.taskmanager.edit.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.rusnet.taskmanager.R
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_activity)
+    }
+}

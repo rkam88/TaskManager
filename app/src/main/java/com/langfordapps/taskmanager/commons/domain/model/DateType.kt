c00fba9469ecd2e05e88967b9cd41c3950f8efdc
Roman Kamyshnikov <rkam88@gmail.com>
2022-05-03 22:26:04 +0300

    Delete old files

--- app/src/main/java/com/langfordapps/taskmanager/commons/domain/model/DateType.kt
+++
@@ -1,7 +0,0 @@
-package com.langfordapps.taskmanager.commons.domain.model
-
-enum class DateType {
-    START_DATE,
-    END_DATE,
-    ALARM_DATE
-}
--- app/src/main/java/com/langfordapps/taskmanager/tasksdisplay/presentation/TasksDisplayEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.langfordapps.taskmanager.tasksdisplay.presentation
-
-sealed class TasksDisplayEvent {
-    data class ShowConfirmationDialog(val dialogTitle: String, val dialogTag: String) : TasksDisplayEvent()
-    object FinishActionMode : TasksDisplayEvent()
-    object ShowNoTasksToDeleteMessage : TasksDisplayEvent()
-}
